30249bbfb2d7189c015910ff0de86ecb0cf663b7
jndoug01 <jndoug01@louisville.edu>
2015-04-29 19:11:20 -0400

    last minute fix

--- client/app/order/order.controller.js
+++ client/app/order/order.controller.js
@@ -1,6 +1,27 @@
 'use strict';
 
 angular.module('cupcakeStoreApp')
-  .controller('OrderCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('OrderCtrl', function ($scope, $http, socket) {
+    $scope.orders = [];
+
+    $http.get('/api/orders').success(function(orders) {
+      $scope.orders = orders;
+      socket.syncUpdates('order', $scope.orders);
+    });
+
+    $scope.addOrder = function() {
+      if($scope.newOrder === '') {
+        return;
+      }
+      $http.post('/api/orders', { name: $scope.newOrder });
+      $scope.newThing = '';
+    };
+
+    $scope.deleteOrder = function(order) {
+      $http.delete('/api/orders/' + order._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('order');
+    });
   });
