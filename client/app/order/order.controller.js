1de2f47060f82f800c1a7b8aed071b7c0d147612
jndoug01 <jndoug01@louisville.edu>
2015-04-29 17:20:22 -0400

    updated order form controller

--- client/app/order/order.controller.js
+++ client/app/order/order.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('cupcakeStoreApp')
-  .controller('OrderCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('OrderCtrl', function ($scope, $http, socket, User) {
+    $scope.Orders = [];
+
+    $http.get('/api/orders').success(function(orders) {
+      $scope.orders = orders;
+      socket.syncUpdates('order', $scope.orders);
+    });
+
+    $scope.addOrder = function() {
+      if($scope.newOrder === '') {
+        return;
+      }
+      $http.post('/api/orders', { name: $scope.newOrder });
+      $scope.newOrder = '';
+    };
   });
